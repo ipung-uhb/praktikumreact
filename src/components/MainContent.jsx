72fca97d925ec8da078b597641a9fb5e38a2e12b
ipung-uhb <purwono@uhb.ac.id>
2024-06-12 21:45:53 +0700

    create maincontent file

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FunctionalComponent from './FunctionalComponent';
+import ClassComponent from './ClassComponent';
+
+const MainContent = () => {
+    return (
+        <main>
+            <FunctionalComponent />
+            <ClassComponent />
+        </main>
+    );
+}
+
+export default MainContent;

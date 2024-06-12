2a116a789f7c5f1b4f9e4b51a0c1a2ee39ea4555
ipung-uhb <purwono@uhb.ac.id>
2024-06-12 22:19:39 +0700

    modified a functionalcomponent with add + and - button with specified input number to change count value

--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -4,13 +4,35 @@ const FunctionalComponent = (props) => {
     const numvalue = parseInt(props.numvalue)
     const [count, setCount] = useState(numvalue);
 
+    const [inputValue, setInputValue] = useState(0);
+
+    const handleInputChange = (event) => {
+        setInputValue(Number(event.target.value));
+    };
+
+    const increment = () => {
+        setCount(count + inputValue);
+    };
+
+    const decrement = () => {
+        setCount(count - inputValue);
+    };
+
     return (
         <div>
             <h2>This is a Functional Component</h2>
             <p><p>{props.message}</p></p>
 
             <p>Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={increment}>+</button>
+            <input
+                type="number"
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder="Enter a number"
+            />
+            
+            <button onClick={decrement}>-</button>
         </div>
 
     )

8f0b7ceabdc0df5a994fffcfdc8c3222936ddfe2
ipung-uhb <purwono@uhb.ac.id>
2024-06-12 21:42:30 +0700

    create classcomponent file

---
+++ src/components/ClassComponent.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ClassComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This is a Class Component</h2>
+                <p>Class components can have their own methods and lifecycle hooks.</p>
+            </div>
+        );
+    }
+}
+
+export default ClassComponent;

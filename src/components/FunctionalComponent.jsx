82d244b463bf23f1622823a937b314775f3754c1
ipung-uhb <purwono@uhb.ac.id>
2024-06-12 21:40:12 +0700

    create file functionalcomponent

---
+++ src/components/FunctionalComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FunctionalComponent = () => {
+  return (
+    <div>
+      <h2>This is a Functional Component</h2>
+      <p>Functional components are simple and easy to use.</p>
+    </div>
+
+  )
+}
+
+export default FunctionalComponent
